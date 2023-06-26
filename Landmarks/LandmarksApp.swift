9e9dcc1e5ea5037b7ddd9280fe2b12d184a2f5dd
AntonyBil <beletskiy.ant@gmail.com>
2023-06-26 20:03:22 +0300

    Apple SwiftUI Tutorial

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by apple on 26.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
